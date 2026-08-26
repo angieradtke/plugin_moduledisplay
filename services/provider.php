7dbb58a766efbafcc2c8eb8658b36595768ec2e7
Angie Radtke <a.radtke@derauftritt.de>
2026-08-26 11:33:29 +0200

    Changed to lazy -> thanks Herman

--- services/provider.php
+++ services/provider.php
@@ -15,50 +15,51 @@
 use Joomla\CMS\Extension\PluginInterface;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Database\DatabaseInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
-use Joomla\Database\DatabaseInterface;
-use Joomla\Event\DispatcherInterface;
 use Joomla\Plugin\System\ModuleDisplay\Extension\ModuleDisplay;
 
 return new class () implements ServiceProviderInterface {
-    /**
-     * Registers the service provider with a DI container.
-     *
-     * @param   Container  $container  The DI container.
-     *
-     * @return  void
-     *
-     * @since   1.0.0
-     */
-    public function register(Container $container): void
-    {
-        $container->set(
-            PluginInterface::class,
-            function (Container $container) {
-                $dispatcher = $container->get(DispatcherInterface::class);
-                $plugin = new ModuleDisplay(
-                    $dispatcher,
-                    (array) PluginHelper::getPlugin('system', 'moduledisplay')
-                );
-                $plugin->setApplication(Factory::getApplication());
-                $plugin->setDatabase($container->get(DatabaseInterface::class));
-
-                /*
-                 * Die Sprachdatei wird zusätzlich hier geladen: Bei
-                 * Service-Provider-Plugins läuft der Konstruktor (und damit
-                 * $autoloadLanguage), bevor setApplication() aufgerufen wurde.
-                 * Beide Wege zusammen stellen sicher, dass die Übersetzungen
-                 * im Modulformular ankommen.
-                 */
-                try {
-                    $plugin->loadLanguage();
-                } catch (\Throwable $e) {
-                    // Ohne Übersetzung lauffähig bleiben.
-                }
-
-                return $plugin;
-            }
-        );
-    }
+	/**
+	 * Registers the service provider with a DI container.
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0.0
+	 */
+	public function register(Container $container): void
+	{
+		$container->set(
+			PluginInterface::class,
+			$container->lazy(
+				ModuleDisplay::class,
+				function (Container $container) {
+					$plugin = new ModuleDisplay(
+						(array) PluginHelper::getPlugin('system', 'moduledisplay')
+					);
+					
+					$plugin->setApplication(Factory::getApplication());
+					$plugin->setDatabase($container->get(DatabaseInterface::class));
+					
+					/*
+					 * Die Sprachdatei wird zusätzlich hier geladen: Bei
+					 * Service-Provider-Plugins läuft der Konstruktor (und damit
+					 * $autoloadLanguage), bevor setApplication() aufgerufen wurde.
+					 * Beide Wege zusammen stellen sicher, dass die Übersetzungen
+					 * im Modulformular ankommen.
+					 */
+					try {
+						$plugin->loadLanguage();
+					} catch (\Throwable $e) {
+						// Ohne Übersetzung lauffähig bleiben.
+					}
+					
+					return $plugin;
+				}
+			)
+		);
+	}
 };
